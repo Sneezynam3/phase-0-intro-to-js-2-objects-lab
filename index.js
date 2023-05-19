7a6f9dddc38aee6008cf87cbdce07512d2b2f5e9
James Snell <vulpes3.1415@yahoo.com>
2023-05-19 10:12:29 -0600

    phase-0-intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+//Assign employee variable to object containing name and streetAddress.
+var employee = {
+    name: 'James', 
+    streetAddress: '7059 Windimere St'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    var newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+}
